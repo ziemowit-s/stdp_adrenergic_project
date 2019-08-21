e96cfe7072259605d5ca56d1544a3cae4c88c0ec
Ziemowit Slawinski <ziemowit.slawinski@gmail.com>
2019-08-21 17:32:46 +0200

    minor changes

--- ltp_models/dimentionality_reduction.py
+++ ltp_models/dimentionality_reduction.py
@@ -1,13 +1,36 @@
 import argparse
+from collections import Iterable
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, NMF
 from sklearn.preprocessing import MinMaxScaler
-
-# idx = header.index("Ca")
-# d[:, idx]
 from ltp_models.utils import get_data
+
+
+def plot(name, molecules, data, header):
+    c = get_concentration(data, header, molecules=molecules, norm=True, sum_cols=True)
+    plt.plot(c, label=name)
+
+
+def reduce_and_plot(reduction_type, n_components, data):
+    reduction_type = reduction_type.lower().strip()
+    if reduction_type == 'pca':
+        r = PCA
+    elif reduction_type == 'nmf':
+        r = NMF
+    else:
+        raise TypeError("Wrong type of reduction class. Allowed: PCA, NMF.")
+
+    data = MinMaxScaler().fit_transform(data)
+
+    f = r(n_components=n_components)
+    c = f.fit_transform(data)
+    c = MinMaxScaler().fit_transform(c)
+
+    for i in range(0, c.shape[1]):
+        plt.plot(c[:, i], label='%s_%s' % (reduction_type, i))
 
 
 def exclude(data, header, molecules):
@@ -19,7 +42,11 @@ def exclude(data, header, molecules):
         tuple(data, header) after removal
     """
     idxs = [header.index(m) for m in molecules]
-    return np.delete(data, idxs, axis=1), np.delete(header, idxs).tolist()
+
+    data = np.delete(data, idxs, axis=1)
+    header = np.delete(header, idxs).tolist()
+
+    return data, header
 
 
 def get_concentration(data, header, molecules, sum_cols=False, norm=False):
@@ -68,24 +95,20 @@ if __name__ == '__main__':
     data, header = get_data(folder=args.folder, prefix=args.prefix, trials=args.trials, morpho=args.morphology)
     data = prepare_trails(data, agregation=args.agregation)
 
-    to_exclude = [h for h in header if "out" in h.lower()]
-    to_exclude.append('time')
+    to_exclude = ['time', 'Ca']
+    to_exclude.extend([h for h in header if "out" in h.lower()])
+    to_exclude.extend([h for h in header if "buf" in h.lower()])
 
     data, header = exclude(data, header, molecules=to_exclude)
     print(header)
 
-    f = PCA(n_components=args.component_number)
-    c = f.fit_transform(MinMaxScaler().fit_transform(data))
+    reduce_and_plot('nmf', n_components=3, data=data)
 
-    # plot normalized PCA components
-    c = MinMaxScaler().fit_transform(c)
-    plt.plot(c[:, 0], label='Component_1')
-    plt.plot(c[:, 1], label='Component_2')
-
-    # plot normalized concentrations
-    plt.plot(get_concentration(data, header, molecules="CKp CKpCaMCa4", norm=True, sum_cols=True), label='CKp')
-    plt.plot(get_concentration(data, header, molecules="pbAR ppbAR pppbAR ppppbAR", norm=True, sum_cols=True), label='pbAR')
-    plt.plot(get_concentration(data, header, molecules="PKA", norm=True), label='PKA')
+    plot("Gibg", molecules="Gibg", data=data, header=header)
+    plot("PKA", molecules="PKA", data=data, header=header)
+    plot("CKp", molecules="CKp CKpCaMCa4", data=data, header=header)
+    plot("CaMCa", molecules="CaMCa2 CaMCa4", data=data, header=header)
+    plot("pbAR", molecules="pbAR ppbAR pppbAR ppppbAR", data=data, header=header)
 
     plt.legend(loc='best')
     plt.show()
