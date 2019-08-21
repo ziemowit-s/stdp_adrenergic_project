5f923c67c3bcba0c15f09531053c140d92d3acef
Ziemowit Slawinski <ziemowit.slawinski@gmail.com>
2019-08-21 12:43:37 +0200

    refactored dimentionality reduction methods

--- ltp_models/dimentionality_reduction.py
+++ ltp_models/dimentionality_reduction.py
@@ -8,46 +8,74 @@ from sklearn.preprocessing import MinMaxScaler
 # d[:, idx]
 from ltp_models.utils import get_data
 
-COMPONENT = 0
+
+def prepare_trails(data, agregation):
+    """
+    :param data:
+    :param agregation:
+        avg or concat
+    :return:
+    """
+    if agregation == "concat":
+        data = data.reshape([data.shape[0], data.shape[1] * data.shape[2]])
+    elif agregation == "avg":
+        data = np.average(data, axis=2)
+    else:
+        raise NameError("Allowed trial agregation are: 'avg', 'concat'.")
+
+    return data
+
+
+def pca(data, header, comp_num, importance_component_num=1):
+    ic = importance_component_num - 1
+    f = PCA(n_components=comp_num)
+    c = f.fit_transform(data)
+
+    a = c[:, 0].reshape(len(c), 1)
+    b = f.components_[ic, :].reshape(f.components_.shape[1], 1)
+    avg_importance = np.average(np.dot(a, b.T), axis=1).tolist()
+
+    return f, c, list(zip(header, avg_importance))
+
+
+def nmf(data, header, comp_num, importance_component_num=1, init='random', random_state=33):
+    ic = importance_component_num - 1
+    f = NMF(n_components=comp_num, init=init, random_state=random_state)
+    c = f.fit_transform(data)
+
+    avg_importance = c[:, ic]
+
+    return f, c, list(zip(header, avg_importance))
+
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("--folder", default=None)
     ap.add_argument("--prefix", required=True)
-    ap.add_argument("--component_number", required=True, type=int)
     ap.add_argument("--morphology", nargs='+', required=True)
-
-    ap.add_argument("--method", help="pca or nnmf. default=nmf", default='nmf')
-    ap.add_argument("--trial_agregation", help="avg or concat. default=concat", default='concat')
+    ap.add_argument("--component_number", required=True, type=int)
+    ap.add_argument("--agregation", help="Many trial agregation type: avg, concat. default=concat", default='concat')
 
     args = ap.parse_args()
 
     for m in args.morphology:
         header, data = get_data(folder=args.folder, prefix=args.prefix, morpho=m, no_time=True)
 
-        if args.trial_agregation == "concat":
-            data = data.reshape([data.shape[0], data.shape[1] * data.shape[2]])
-        elif args.trial_agregation == "avg":
-            data = np.average(data, axis=2)
-        else:
-            raise NameError("Allowed trial agregation are: 'avg', 'concat'.")
-
+        data = prepare_trails(data, agregation=args.agregation)
         data = MinMaxScaler().fit_transform(data)
 
-        f = PCA(n_components=args.component_number)
-        c = f.fit_transform(data)
-        print('PCA variance explained:', m, sum(f.explained_variance_ratio_), f.explained_variance_ratio_)
+        pca_f, pca_c, pca_i = pca(header=header, data=data, comp_num=args.component_number)
+        nmf_f, nmf_c, nmf_i = nmf(header=header, data=data, comp_num=args.component_number)
 
-        importance_pca = sorted(
-            list(zip(header, np.average(np.dot(c[:, COMPONENT].reshape(len(c), 1), f.components_[COMPONENT, :].reshape(f.components_.shape[1], 1).T),
-                                        axis=1).tolist())), key=lambda x: -x[1])
+        print('PCA variance explained:', round(sum(pca_f.explained_variance_ratio_)*100, 5))
+        print('NMF variance explained:', round(np.sum(nmf_c), 4))
+        print()
 
-        f = NMF(n_components=args.component_number, init='random', random_state=33)
-        c = f.fit_transform(data)
-        print('NMF variance explained:', m, np.sum(c), sum(c))
+        pca_i = sorted(pca_i, key=lambda x: -x[1])
+        nmf_i = sorted(nmf_i, key=lambda x: -x[1])
 
-        importance_nmf = sorted(list(zip(header, c[:, COMPONENT])), key=lambda x: -x[1])
-
-        print('%s COMPONENT IMPORTANCE FOR:' % (COMPONENT+1), 'PCA', 'NMF')
-        for i in zip(importance_pca, importance_nmf):
-            print(i)
+        print("PCA / NMF")
+        for i in list(zip(pca_i, nmf_i))[:10]:
+            pcaval = "%s:%s" % (i[0][0], round(i[0][1], 4))
+            nmfval = "%s:%s" % (i[1][0], round(i[1][1], 4))
+            print("%s\t%s" % (pcaval, nmfval))
